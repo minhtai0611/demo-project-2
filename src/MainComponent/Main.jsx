a364c23c102002dbba770c28eaed1db8a45cd349
Tai Minh <minhtainth06111999@gmail.com>
2024-04-25 21:14:44 +0700

    demo-project-2

--- src/MainComponent/Main.jsx
+++ src/MainComponent/Main.jsx
@@ -1,30 +1,34 @@
 import styled from "./Main.module.css";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo-game.png"
 export default function Main() {
     return (
         <>
             <header className={styled.header}>
-                <img alt="logo game" className={styled.img} />
+                <img src={logo} alt="logo game" className={styled.img} />
                 <nav className={styled.nav}>
-                    <Link to="/">
+                    <Link to="/" className={styled.link + " " + styled["shippori-antique-b1-regular"]}>
                         Store
                     </Link>
-                    <Link to="/community">
+                    <Link to="/community" className={styled.link + " " + styled["shippori-antique-b1-regular"]}>
                         Community
                     </Link>
-                    <Link to="/information">
+                    <Link to="/information" className={styled.link + " " + styled["shippori-antique-b1-regular"]}>
                         Information
                     </Link>
-                    <Link to="/support">
+                    <Link to="/support" className={styled.link + " " + styled["shippori-antique-b1-regular"]}>
                         Support
                     </Link>
-                    <Link to="/login">
+                    <Link to="/login" className={styled.link + " " + styled["shippori-antique-b1-regular"] + " " + styled.auth}>
                         Login
                     </Link>
-                    <Link to="/register">
+                    <Link to="/register" className={styled.link + " " + styled["shippori-antique-b1-regular"] + " " + styled.auth}>
                         Register
                     </Link>
                 </nav>
+                <button type="button" className={styled.threeline + " " + styled["shippori-antique-b1-regular"]}>
+                    ☰
+                </button>
             </header>
             <footer>
 
